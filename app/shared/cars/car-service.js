37cca17f8a9ae82d3cb66292dae3e16028e3c8f6
Roberto Rielo <roberto910907@gmail.com>
2018-12-23 16:32:15 -0500

    Initial Commit

---
+++ app/shared/cars/car-service.js
@@ -0,0 +1,20 @@
+import Car from "./car-model";
+
+const editableProperties = [
+    "doors",
+    "imageUrl",
+    "luggage",
+    "name",
+    "price",
+    "seats",
+    "transmission",
+    "class"
+];
+
+function CarService() {
+    if (CarService._instance) {
+        throw new Error("Use CarService.getInstance() instead of new.");
+    }
+
+    this._cars = [];
+}
